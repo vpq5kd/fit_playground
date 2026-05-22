97d7c0f6acc252a576322c2352c74011969e8589
sophia spaner <vpq5kd@virginia.edu>
2026-05-22 02:12:17 -0400

    made phase study

--- phase_study.py
+++ phase_study.py
@@ -23,6 +23,7 @@ parser.add_argument('--show_interpolators', action='store_true')
 parser.add_argument('--no_visualizations', action='store_true')
 args = parser.parse_args()
 
+
 fp = folder_processor()
 df, df_scint_photons, df_cheren_photons = fp.process_folder(args.folder)
 
@@ -42,53 +43,71 @@ def handle_visualizations(data_x, data_y, sp, cp):
         else:
             visualizer.visualize_data_without_fit(data_x, data_y)
 
-#scint_time_correction = (300-300*(np.exp(-2/3)))/(300)
+def fit_data(data_x, data_y, data_x_coord, data_y_coord, event, step, start_point):
 
-cmap = plt.get_cmap('cool')
-plot_tuple_array = []
-step_array = [1,2,4,8,16,32]
-for step_index, step in enumerate(step_array):
-    print(f"Running study on {5/step:.3f}gHz")
+    scint_photon_guess, cheren_photon_guess, expected_amplitude, scint_photons, cheren_photons = sf.fit_data(data_x, data_y, data_x_coord, data_y_coord, event, args.particle, args.energy, step, start_point)
+
+
+    scint_expected_percent  = 100*(scint_photons)/(scint_photons + cheren_photons)
+    cheren_expected_percent = 100*(cheren_photons)/(scint_photons + cheren_photons) 
     
-    cheren_expected_percents = []
-    cheren_fit_percents = []
+    total_fit_guess = scint_photon_guess + cheren_photon_guess
+    fit_scint_fraction = scint_photon_guess/(total_fit_guess)
+    fit_cheren_fraction = cheren_photon_guess/(total_fit_guess)
+    print("-"*50)
+    print(f"Event: {event}")
+    print(f"Data Amplitude: {expected_amplitude}")
+    print(f"Scint fit percentage: {(fit_scint_fraction*100):.3f}")
+    print(f"Cheren fit percentage: {(fit_cheren_fraction*100):.3f}")
+    print(f"Scint expected percentage: {scint_expected_percent:.3f}")
+    print(f"Cheren expected percentage: {cheren_expected_percent:.3f}")
+    print(f"Scint photon amount: {scint_photons}")
+    print(f"Cheren photon amount: {cheren_photons}")
+    print(f"ix: {data_x_coord}")
+    print(f"iy: {data_y_coord}")
 
-    for event in sf.df["event"].unique():
+    handle_visualizations(data_x, data_y, scint_photon_guess, cheren_photon_guess)
 
-        data = sf.get_signal_data_by_event(event)
+    return fit_cheren_fraction, cheren_expected_percent
 
-        for data_x, data_y, data_x_coord, data_y_coord in data:
+def main():
 
-            scint_photon_guess, cheren_photon_guess, expected_amplitude, scint_photons, cheren_photons = sf.fit_data(data_x, data_y, data_x_coord, data_y_coord, event, args.particle, args.energy, step)
+    cmap = plt.get_cmap('cool')
+    start_point_array = np.arange(15)
+    step_array = [8,16,32]
+    frequency_plot_arrays = []
 
+    for step_index, step in enumerate(step_array):
+        
+        plot_tuple_array = []
 
-            scint_expected_percent  = 100*(scint_photons)/(scint_photons + cheren_photons)
-            cheren_expected_percent = 100*(cheren_photons)/(scint_photons + cheren_photons) 
+        for start_point_index, start_point in enumerate(start_point_array):
+
+            print(f"Running study on {5/step:.3f}gHz with start point {0.2*start_point:.3f}ns")
             
-            total_fit_guess = scint_photon_guess + cheren_photon_guess
-            fit_scint_fraction = scint_photon_guess/(total_fit_guess)
-            fit_cheren_fraction = cheren_photon_guess/(total_fit_guess)
-            print("-"*50)
-            print(f"Event: {event}")
-            print(f"Data Amplitude: {expected_amplitude}")
-            print(f"Scint fit percentage: {(fit_scint_fraction*100):.3f}")
-            print(f"Cheren fit percentage: {(fit_cheren_fraction*100):.3f}")
-            print(f"Scint expected percentage: {scint_expected_percent:.3f}")
-            print(f"Cheren expected percentage: {cheren_expected_percent:.3f}")
-            print(f"Scint photon amount: {scint_photons}")
-            print(f"Cheren photon amount: {cheren_photons}")
-            print(f"ix: {data_x_coord}")
-            print(f"iy: {data_y_coord}")
+            cheren_expected_percents = []
+            cheren_fit_percents = []
 
-            handle_visualizations(data_x, data_y, scint_photon_guess, cheren_photon_guess)
+            for event in sf.df["event"].unique():
 
-            cheren_fit_percents.append((fit_cheren_fraction)*100)
-            cheren_expected_percents.append(cheren_expected_percent)
-    
-    label = f"{5/step:.3f}gHz"
-    color = cmap(step_index/(len(step_array)-1))
-    plot_tuple = (cheren_fit_percents, cheren_expected_percents, label, color)
-    plot_tuple_array.append(plot_tuple)
+                data = sf.get_signal_data_by_event(event)
 
-np.savez("sr_study_data.npz", plot_tuple_array=np.array(plot_tuple_array, dtype=object))
-visualizer.visualize_rate_sampling_histograms(plot_tuple_array, bins=15)
+                for data_x, data_y, data_x_coord, data_y_coord in data:
+                        
+                    fit_cheren_fraction, cheren_expected_percent = fit_data(data_x, data_y, data_x_coord, data_y_coord, event, step, start_point)
+
+                    cheren_fit_percents.append((fit_cheren_fraction)*100)
+                    cheren_expected_percents.append(cheren_expected_percent)
+            
+            title = f"{5/step:.3f}gHz"
+            label = f"{0.2*start_point:.3f} (ns)"
+            color = cmap(start_point_index/(len(start_point_array)-1))
+            plot_tuple = (cheren_fit_percents, cheren_expected_percents, label, color, title)
+            plot_tuple_array.append(plot_tuple)
+        
+        frequency_plot_arrays.append(plot_tuple_array)
+        
+    np.savez("new_waveform_phase_study_data.npz", frequency_plot_arrays=np.array(frequency_plot_arrays, dtype=object))
+    visualizer.visualize_phase_effect_histograms(frequency_plot_arrays)
+
+main()
